62e817d18b4ac57571edd3d8e17dd83ed6360cff
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-09 20:20:45 +0600

    comment added in expanse receipt part

--- src/Pages/ExpanseReceipt/ExpanseReceipt.jsx
+++ src/Pages/ExpanseReceipt/ExpanseReceipt.jsx
@@ -86,17 +86,29 @@ const ExpanseReceipt = () => {
 
                 </div>
 
+
+
                 {/* Select expanse  (2nd part) */}
 
                 <div className=' w-[340px] '>
 
 
-                    <div className='border mt-7  mx-auto  p-4 rounded '>
-                        <h4 className='border'>Select Expanse</h4>
-                        <button className='border btn btn-primary'>+Add</button>
+                    <div className='border mt-7  mx-auto  rounded '>
+                        <h4 className='text-lg text-black pl-3 py-2 border font-bold'>Select Expense</h4>
+
+                        <hr className='p-0 m-0 ' />
+
+                        <div className=" py-2 w-full text-center ">
+                            <button className=" bg-[#5bc0de] w-[300px]   h-[40px]   text-white text-xl">
+                                +Add
+                            </button>
+                        </div>
+                        
 
 
                     </div>
+
+                    {/* receipt notes */}
 
                     <div className='border mt-7  mx-auto  p-4 rounded '>
                         <h4 className='border'>Receipt Notes</h4>
