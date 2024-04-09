b12d182b1d73260561ecb2ac74542e3b62a8cf70
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-09 18:50:59 +0600

    table add for receipt list

--- src/Pages/ExpanseReceipt/ExpanseReceipt.jsx
+++ src/Pages/ExpanseReceipt/ExpanseReceipt.jsx
@@ -5,43 +5,87 @@ const ExpanseReceipt = () => {
     return (
         <>
 
-            {/* Expanse receive list (1st part) */}
-        
-
-            <div className='border mt-7 w-[1000px]  mx-auto  p-4 rounded shadow-2xl'>
+            <div className='flex w-[1350px] gap-10'>
 
 
-                <div className='flex '>
-                    <img src="https://cashbaksho.com/backend/billing_invoice_logo.png" alt="" />
-                    <div className=''>
-                        <p>Date : 
 
-                        <input type="date" placeholder="Type here" className="input input-bordered " />
+                {/* Expanse receive list (1st part) */}
 
-                        </p>
 
-                        <p>Receiver : 
-                            <input type="text" placeholder="Receiver" className="input input-bordered  " />
+                <div className='border mt-7 w-[1000px]  mx-auto  p-4 rounded shadow-2xl'>
 
-                        </p>
+
+                    <div className='flex '>
+                        <img src="https://cashbaksho.com/backend/billing_invoice_logo.png" alt="" />
+                        <div className=''>
+                            <p>Date :
+
+                                <input type="date" placeholder="Type here" className="input input-bordered " />
+
+                            </p>
+
+                            <p>Receiver :
+                                <input type="text" placeholder="Receiver" className="input input-bordered  " />
+
+                            </p>
+                        </div>
+
                     </div>
+
+
+
+                    <h1 className='text-3xl mt-5'>Expanse Receipt List</h1>
+
                    
+
+                    <table className='w-full'>
+                        <tr>
+                            <td className='text-black border text-center font-extrabold text-lg'>SN</td>
+                            <td className='text-black border text-center font-extrabold text-lg'>Expense</td>
+                            <td className='text-black border text-center font-extrabold text-lg'>Details</td>
+                            <td className='text-black border text-center font-extrabold text-lg'>Amount</td>
+                        </tr>
+                    </table>
                 </div>
 
-              
+                {/* Select expanse  (2nd part) */}
 
-                <h1 className='text-3xl mt-5'>Expanse Receipt List</h1>
+                <div className=' w-[340px]'>
 
-                <div className='border'>
+
+                    <div className='border mt-7  mx-auto  p-4 rounded '>
+                        <h4 className='border'>Select Expanse</h4>
+                        <button className='border btn btn-primary'>+Add</button>
+
+
+                    </div>
+
+                    <div className='border mt-7  mx-auto  p-4 rounded '>
+                        <h4 className='border'>Receipt Notes</h4>
+
+                        <p>Notes(required)</p>
+
+                        <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
+
+
+                      <div>
+                            <button className='border btn btn-primary'>+Add</button>
+
+                      </div>
+
+                    </div>
+
+                
 
                 </div>
+
+
+
             </div>
 
-            {/* Select expanse  (2nd part) */}
-        
-        
+
         </>
-       
+
     );
 };
 
