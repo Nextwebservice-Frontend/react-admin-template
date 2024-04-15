8f5fdca20093cc2d9b6244b6da671f881957158f
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-04-15 16:43:15 +0600

    shortcut  popup edited

--- src/Components/dashboard/Navber/Shortcut_pop_up/ShortcutPopUp.jsx
+++ src/Components/dashboard/Navber/Shortcut_pop_up/ShortcutPopUp.jsx
@@ -1,21 +1,23 @@
 
 import { BiCustomize } from "react-icons/bi";;
 import { ShortcutPopUpMenu } from "../../../../Utility/Navber/Shortcut_pop_up_menus/ShortcutPopUpMenus";
-const ShortcutPopUp = ({ showSortcutPopUp,setshowSortcutPopUp }) => {
+const ShortcutPopUp = ({ showSortcutPopUp, setshowSortcutPopUp }) => {
 
     return (
-        <div className={`absolute shadow-lg rounded-lg border lg:right-24 md:right-20 right-0 z-20 -top-12 mt-10 bg-white ${showSortcutPopUp ? "popup" : "hidden"}  max-h-[90vh] overflow-y-auto`}>
-            <span className='flex justify-between items-center min-w-80 max-w-80 sm:max-w-96 sm:min-w-96 text-lg p-3 text-gray-600 font-semibold opacity-70'>
+        <div className={`absolute shadow-lg rounded-lg border lg:right-24 md:right-20 right-0 z-20 -top-12 mt-10 bg-white ${showSortcutPopUp ? "popup" : "hidden"}  max-h-[90vh] overflow-y-hidden`}>
+            <span className='flex justify-between items-center min-w-80 max-w-80 sm:max-w-80 sm:min-w-80 text-lg p-3 text-gray-600 font-semibold opacity-70'>
                 Shortcuts
                 <button>
                     <BiCustomize className='text-2xl' />
                 </button>
             </span>
-            <div className='grid grid-cols-2 gap-0 justify-center items-center text-gray-500 mt-2 '>
+            <div id="NotificationPopUp" className='grid grid-cols-2 gap-0 justify-center overflow-y-scroll items-center text-gray-500 mt-2 max-h-[312px]'>
                 {
-                    ShortcutPopUpMenu.map((item ,index)=>
-                        <button onClick={()=>setshowSortcutPopUp(false)} key={index} className='flex justify-center items-center font-semibold flex-col border py-4 gap-2 tracking-[.8px] hover:bg-slate-100 transition-all'>
-                            <item.icon className='text-xl' />
+                    ShortcutPopUpMenu.map((item, index) =>
+                        <button onClick={() => setshowSortcutPopUp(false)} key={index} className='flex justify-center items-center font-semibold flex-col border py-2 gap-1 tracking-[.8px] hover:bg-slate-100 transition-all text-sm'>
+                            <span className="bg-[#EBEAEC]  rounded-full  p-3">
+                                <item.icon className='text-xl ' />
+                            </span>
                             {item.title}
                             <span className="-mt-[6px] font-normal block opacity-70">{item?.subTitle}</span>
                         </button>)
