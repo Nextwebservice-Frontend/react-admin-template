a48b0b47c49c40cea5803d38f33f32c14ba901ab
Tanha <sanjidatanha09@gmail.com>
2024-04-20 17:54:30 +0600

    bgy

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -7,7 +7,7 @@ import ServiceList from "../Pages/Services/ServiceList/ServiceList";
 import YearlyInvoice from "../Pages/Reports/YearlyInvoice/YearlyInvoice";
 import GeneralSetting from "../Pages/Settings/GeneralSetting/GeneralSetting";
 import Button from "../Pages/Button/Button";
-import Provider from "../Pages/Others/Provider";
+// import Provider from "../Pages/Others/Provider";
 import ExpenseReceipt from "../Pages/ExpenseReceipt/ExpenseReceipt/ExpenseReceipt";
 import CategoryList from "../Pages/Expense/CategoryList/CategoryList";
 import ExpenseList from "../Pages/Expense/ExpenseList/ExpenseList";
