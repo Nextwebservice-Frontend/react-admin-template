702120573ff9db3c94bdcce60021bd94983c2d25
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-04-22 19:28:02 +0600

    Update SIderberNavLinks.js

--- src/Utility/Sideber/SIderberNavLinks.js
+++ src/Utility/Sideber/SIderberNavLinks.js
@@ -151,8 +151,6 @@ export const SIderberNavLinks = [
                         access: 'Collapsed-menu',
                         subMenu: false
                     },
-                    
-                  
                 ]
             },   
             
